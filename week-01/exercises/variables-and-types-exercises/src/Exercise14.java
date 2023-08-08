ae30c703a5642d345ce883318092de6e335a4aa5
Carlos del Orbe <carlos.e.delorbe@gmail.com>
2023-08-08 12:00:59 -0400

    changed exercise 14 during lecture

--- week-01/exercises/variables-and-types-exercises/src/Exercise14.java
+++ week-01/exercises/variables-and-types-exercises/src/Exercise14.java
@@ -1,8 +1,8 @@
 public class Exercise14 {
     public static void main(String[] arg) {
         int gradeLevel = 12;
-        boolean isASenior = gradeLevel >= 12;
-        boolean isInterestedInVoulenteering = true;
+        boolean isASenior = gradeLevel == 12;
+        boolean isInterestedInVoulenteering;
         boolean shouldSendVoulenteerInfo = isASenior && isInterestedInVoulenteering;
 
         System.out.println(shouldSendVoulenteerInfo);
